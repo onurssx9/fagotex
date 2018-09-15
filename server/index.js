bce488d0251a34760c56c742f03f5ca3ee2619e5
mvpolcay <olcaymhmmet@gmail.com>
2018-09-15 03:55:02 +0300

    api defined

--- server/index.js
+++ server/index.js
@@ -13,9 +13,12 @@ const ngrok =
     : false;
 const { resolve } = require('path');
 const app = express();
+const horizon = require('./horizon');
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
+
+app.use('/api', horizon);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
